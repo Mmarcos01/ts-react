9e404bff481d27b859fe92ca4f5f6c8dfdadcddb
mmarcos <marie.marcos@slalom.com>
2023-02-02 13:56:39 -0800

    change event

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React, {FC} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [num, setNum] = useState<number>(0);
+  const [list, setList] = useState([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "task") {
+      setTask(e.target.value)
+    } else {
+      setNum(Number(e.target.value));
+    }
+  };
+
   return (
     <div className="App">
       <div className="header">
-        <input type="text" placeholder="Item..." />
-        <input type="number" placeholder="Number" />
+        <input type="text" placeholder="Item..." name="task" onChange={handleChange} />
+        <input type="number" name="number" placeholder="Number" />
         <button>Add Item</button>
       </div>
       <div className="list"></div>
